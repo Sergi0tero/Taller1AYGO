a910b3bc1c0f4f90adcfa95dc173e8eead44635c
Sergi0tero <sergioaotero26@hotmail.com>
2025-10-23 22:18:17 -0500

    Codigo terminado

---
+++ virtualizacionydocker/src/main/java/com/taller/virtualizacionydocker/VirtualizacionydockerApplication.java
@@ -0,0 +1,13 @@
+package com.taller.virtualizacionydocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualizacionydockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualizacionydockerApplication.class, args);
+	}
+
+}
---
+++ virtualizacionydocker/src/test/java/com/taller/virtualizacionydocker/VirtualizacionydockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller.virtualizacionydocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualizacionydockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
